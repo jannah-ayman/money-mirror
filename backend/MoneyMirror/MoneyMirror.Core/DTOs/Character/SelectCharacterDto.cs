c4432dc8e3cf2f2caa9709985a21d3c6a691e621
Jannah <jannahayman16@gmail.com>
2026-02-05 01:09:53 +0200

    removed unused dtos

---
+++ backend/MoneyMirror/MoneyMirror.Core/DTOs/Character/SelectCharacterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyMirror.Core.DTOs.Character
+{
+    /// <summary>
+    /// DTO for selecting a character.
+    /// </summary>
+    public class SelectCharacterDto
+    {
+        public int CharacterID { get; set; }
+    }
+}
