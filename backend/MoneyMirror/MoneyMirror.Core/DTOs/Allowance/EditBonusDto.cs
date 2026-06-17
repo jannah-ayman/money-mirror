115bd34b87f2ab46d16459c127a53cd6d46a5485
Nancy Saad <Nancy.Saad9583@compit.aun.edu.eg>
2026-06-17 20:49:16 +0300

    handled modifying bonuses and expenses

---
+++ backend/MoneyMirror/MoneyMirror.Core/DTOs/Allowance/EditBonusDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMirror.Core.DTOs.Allowance
+{
+    internal class EditBonusDto
+    {
+    }
+}
---
+++ backend/MoneyMirror/MoneyMirror.Core/DTOs/Expense/UpdateExpenseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMirror.Core.DTOs.Expense
+{
+    internal class UpdateExpenseDto
+    {
+    }
+}
